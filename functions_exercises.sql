1fc4122ddfacd139e025118e3d83a2a21de5f0f9
Mitchel Sandidge <masandidge12@gmail.com>
2020-04-16 11:01:41 -0500

    mySQL functions exercises

---
+++ functions_exercises.sql
@@ -0,0 +1,75 @@
+USE employees;
+
+-- ORDER BY Exercises ---------
+
+SELECT first_name AS 'fn', last_name AS 'last..'
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name;
+
+
+SELECT first_name AS 'fn', last_name AS 'last..'
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name, last_name;
+
+
+SELECT first_name AS 'fn', last_name AS 'last..'
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name, first_name;
+
+
+SELECT emp_no AS 'Emp #', first_name AS 'first', last_name AS 'last..'
+FROM employees
+WHERE last_name LIKE ('E%')
+ORDER BY emp_no;
+
+
+SELECT first_name AS 'fn', last_name AS 'last..'
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name DESC;
+
+
+SELECT emp_no AS 'Emp #', first_name AS 'first', last_name AS 'last..'
+FROM employees
+WHERE last_name LIKE ('E%')
+ORDER BY emp_no DESC;
+
+
+SELECT 'Christmas birthdays, hired first' AS ' ';
+SELECT first_name, last_name, hire_date, birth_date
+FROM employees
+WHERE hire_date LIKE ('199%')
+AND birth_date LIKE ('%12-25%')
+ORDER BY birth_date ASC, hire_date DESC;
+
+
+
+-- Functions Exercise
+
+SELECT CONCAT (first_name, ' ', last_name)
+FROM employees
+WHERE last_name LIKE ('e%e')
+ORDER BY emp_no;
+
+
+SELECT 'Christmas birthdays, hired in the 90''s' AS ' ';
+SELECT first_name, last_name, hire_date, birth_date
+FROM employees
+WHERE hire_date LIKE ('199%')
+  AND birth_date LIKE ('%12-25%')
+ORDER BY hire_date, birth_date;
+
+
+
+SELECT CONCAT(
+               first_name, ' ', last_name, ' has been working here for ',
+               ROUND ((UNIX_TIMESTAMP(hire_date) - CURDATE())/60/60/24/30/12, 0),
+               ' years.'
+           )
+from employees
+WHERE hire_date LIKE ('199%')
+  AND birth_date LIKE ('%12-25%')
+ORDER BY hire_date, birth_date;
