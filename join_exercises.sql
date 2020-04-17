a9d7e03002fe325ebbca6a556d9ebdd7b978bbd2
Mitchel Sandidge <masandidge12@gmail.com>
2020-04-17 12:26:50 -0500

    Join_exercises.sql remastered

--- join_exercises.sql
+++ join_exercises.sql
@@ -38,13 +38,13 @@ WHERE dm.to_date = '9999-01-01' AND e.gender = 'F';
 
 SELECT t.title AS Title, COUNT(*) AS Count
 FROM employees AS e
-         JOIN dept_emp AS dem
-              ON dem.emp_no = e.emp_no
+         JOIN dept_emp AS de
+              ON de.emp_no = e.emp_no
          JOIN departments AS d
-              ON dem.dept_no = d.dept_no
+              ON de.dept_no = d.dept_no
          JOIN titles AS t
               ON e.emp_no = t.emp_no
-WHERE t.to_date = '9999-01-01' AND d.dept_name = 'Customer Service'
+WHERE t.to_date = '9999-01-01' AND d.dept_name = 'Customer Service' AND de.to_date = '9999-01-01'
 GROUP BY t.title;
 
 
@@ -65,13 +65,17 @@ WHERE dm.to_date = '9999-01-01' AND s.to_date = '9999-01-01';
 
 # Bonus: Find the names of all current employees, their department name, and their current manager's name.
 
-SELECT CONCAT(e.first_name, ' ', e.last_name) AS Name, d.dept_name AS Department, CONCAT(e.first_name, ' ', e.last_name) AS Manager
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS Name, d.dept_name AS Department, CONCAT(em.first_name, ' ', em.last_name) AS Manager
 FROM employees AS e
-JOIN dept_emp AS dem
-ON dem.emp_no = e.emp_no
+JOIN dept_emp AS de
+ON de.emp_no = e.emp_no
 JOIN departments AS d
-ON d.dept_no = dem.dept_no
-
-WHERE dem.to_date = '9999-01-01'
+ON d.dept_no = de.dept_no
+JOIN dept_manager AS dm
+ON de.dept_no = dm.dept_no
+JOIN employees AS em
+ON dm.emp_no = em.emp_no
+WHERE de.to_date = '9999-01-01' AND dm.to_date = '9999-01-01'
+ORDER BY d.dept_name
 LIMIT 40;
 
